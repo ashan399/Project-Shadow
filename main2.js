4615514de0ce5f9b2f9f2f4630fbf779f6f51e4e
ashan399 <ashan399@gmail.com>
2020-02-15 17:26:55 -0500

    portfolio

--- main2.js
+++ main2.js
@@ -27,10 +27,32 @@ function type (val){
     if(val>=str.length)
         return;
     setTimeout( () => {
-        console.log(val)
         document.getElementById("typing").innerHTML += str.charAt(val);
         type(val+1);
-    },65);
+    },60);
     
 }
 type(0);
+
+
+// page 2
+
+var tl = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+tl.from("#box", 1, {opacity: 0, scale: 0});
+tl.to("#box", .5, {left: "40%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: "blockquote"
+  })
+    .setTween(tl)
+          .addTo(controller);
+
+var tl2 = new TimelineMax();
+const controller2 = new ScrollMagic.Controller();
+tl2.from("#about", 1, {opacity: 0, scale: 0});
+tl.to("#box", .5, {left: "40%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: "blockquote"
+  })
+    .setTween(tl)
+          .addTo(controller2);
